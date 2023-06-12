ad23a4dea15037d8657f78dbe066d61ac99350c4
Paulina Reus <paularkla@gmail.com>
2023-06-12 10:16:11 +0200

    Zadanie A-kod

---
+++ groupwork_1.py
@@ -0,0 +1,19 @@
+#Zadanie A
+import math
+
+#Funkcja licząca objętość sześcianu i jego pole powierzchn
+
+def szescian(a):
+    p = 6 * pow(a,2)
+    v =pow(a,3)
+    return p, v
+
+
+print(szescian(5))
+
+#funkcja licząca objętość kuli,
+def objetosc_kuli(r):
+    v = 4/3 * math.pi * pow(r,3)
+    return v
+
+print(objetosc_kuli(3))
